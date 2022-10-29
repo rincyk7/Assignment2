9cf076beddbfc927e260ad45af430df0a7fe37e1
rincyk <113142186+rincyk7@users.noreply.github.com>
2022-10-29 17:12:15 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports =
+{
+    //"URI": "mongodb://127.0.0.1:27017/book_store",
+    //"URI": "mongodb+srv://rincyk7:NydhHqKRV7AW1EPJ@clustercomp229.a9gmbpn.mongodb.net/book_store?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://rincyk71:WfddnUvLEyheAUgu@cluster0.5tztha5.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
